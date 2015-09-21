6a9467cc7af7b74449f6ae100b33e6b73d2618de
osquitoOfTheNorth <oscar.menjivar@utoronto.ca>
2015-09-20 23:00:28 -0400

    First Commit

---
+++ type.js
@@ -0,0 +1,167 @@
+
+function Console(configuration){
+    var ShellHasStarted = false;
+    var TextArrayLength;
+    var CurrentLineNumber = 0;
+    var LineIdentifiers = [];
+    //an array of text to type
+    var text;
+    var intervalSpeed;
+    var cursorBlinkSpeed;
+    var target;
+    var textColor;
+    var cursorColor;
+    var currentCursorColor;
+    var typeResponseModeEnabled;
+    //GIANT TODO: CHECK FOR BOUNDS!!
+    if(configuration.text !== undefined)
+    {
+        text = configuration.text;
+        TextArrayLength = text.length;
+        BuildLineIdentifiersArray(TextArrayLength);
+    }
+    if(configuration.target !== undefined)
+    {
+        //a javascript object representing the div that will act as container for shell.
+        target = configuration.target;
+    }
+
+    if(configuration.speed !== undefined)
+    {
+        intervalSpeed = configuration.speed;
+    }
+    else
+    {
+        //default speed is 1 charachter per 200 ms.
+        intervalSpeed = 200    
+    };
+
+    if(configuration.cursorBlinkSpeed !== undefined)
+    {
+        cursorBlinkSpeed = configuration.cursorBlinkSpeed;
+    }
+    else
+    {
+        //cursor will blink every 100 ms.
+        cursorBlinkSpeed = 100;
+    }
+    if(configuration.textColor !== undefined)
+    {
+        textColor = configuration.textColor;
+    }
+    else
+    {
+        textColor = "black";
+    }
+
+    if(configuration.cursorColor !== undefined)
+    {
+        cursorColor = configuration.cursorColor;
+    }
+    else
+    {
+        cursorColor = "gray";
+    }
+
+    this.RunConsole = function(){
+
+        SetupAndRun();
+    };
+
+
+
+    function AddText()
+    {
+            var line = text[CurrentLineNumber];
+            if(line === undefined){
+                //End the recursion array bounds exceeded, place cursor at bottom.
+                ResetCursorMoveToNextLine();
+                PlaceCursorOnDom();
+                animationDone = true;
+                return;
+            }
+            currentLine = line.split('');
+            currentShellIdString = LineIdentifiers[CurrentLineNumber];
+            characterNumber = 0;
+            PlaceTextLineSectionOnDom(currentShellIdString);
+            PlaceCursorOnDom();
+            typeLine(currentLine, currentShellIdString, characterNumber);
+
+    }
+    function getNextCursorColor()
+    {
+
+        if(currentCursorColor == "white")
+        {
+            currentCursorColor = cursorColor;
+        }
+        else
+        {
+            currentCursorColor = "white";
+        }
+        return currentCursorColor;
+    }
+    
+    function BlinkCursor()
+    {
+        cursorbgColor = getNextCursorColor();
+        console.log(cursorbgColor);
+         $("#cursor").animate({backgroundColor:cursorbgColor},{duration:150});
+    }
+
+    function BuildLineIdentifiersArray(NumLines)
+    {
+        for(var i = 0; i < NumLines; i++)
+        {
+            LineIdentifiers.push("ShellLineNumber" + i);
+        }
+    }
+
+    function typeLine(currentLine, currentLineIdentifier, currentCharacterNumber)
+    {
+        if(currentCharacterNumber == currentLine.length)
+        {
+            ResetCursorMoveToNextLine();
+            AddText();
+            return;
+        }
+        else
+        {
+            var c = currentLine[characterNumber];
+            $("#" + currentLineIdentifier).append(c);
+            characterNumber++;
+            window.setTimeout(function() { typeLine(currentLine,currentLineIdentifier,characterNumber); },intervalSpeed);
+        }
+    }
+
+
+    function PlaceCursorOnDom()
+    {
+        target.append("<div style=\"background-color: " + cursorColor + ";\" id=\"cursor\" ></div>");
+    }
+
+    function PlaceTextLineSectionOnDom(currentShellIdString)
+    {
+
+        target.append("<div class=\"shellLine\" style=\" color:" + textColor + "; \" id=\"" + currentShellIdString + "\"></div>");
+    }
+    function SetupAndRun()
+    {
+        target.append(">>   ");
+        target.css("white-space","nowrap");
+        window.setInterval(BlinkCursor, cursorBlinkSpeed);
+        AddText();
+    }
+
+    function ResetCursorMoveToNextLine()
+    {
+        CurrentLineNumber++;
+        $("#cursor").remove();
+        target.append("<br />");
+    }
+
+
+}
+
+
+
